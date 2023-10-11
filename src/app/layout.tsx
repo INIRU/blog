d7d57617a6004136051317c28c6cd4c2f618993c
INIRU <iniru@kakao.com>
2023-10-11 22:09:23 +0900

    start

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '우당탕탕 개발 블로그',
+  description: 'Next.JS Blog Project',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
